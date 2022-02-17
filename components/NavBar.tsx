e0e83895f08118c098293f65b7d3e2bc761ac8cf
starhn87 <starhn87@gmail.com>
2022-02-18 01:17:53 +0900

    NavBar sass 적용

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -21,21 +21,23 @@ export default function NavBar() {
           padding-bottom: 10px;
           box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
             rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+
+          a {
+            font-weight: 600;
+            font-size: 18px;
+          }
+
+          div {
+            display: flex;
+            gap: 10px;
+          }
         }
         img {
           max-width: 100px;
           margin-bottom: 5px;
         }
-        nav a {
-          font-weight: 600;
-          font-size: 18px;
-        }
         .active {
           color: tomato;
-        }
-        nav div {
-          display: flex;
-          gap: 10px;
         }
       `}</style>
     </nav>
